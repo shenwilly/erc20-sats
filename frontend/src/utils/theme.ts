3472d57361830ee69e5b6f0c510bda41eadb0e72
willy <shen.willy95@gmail.com>
2021-06-11 23:21:52 +0700

    add theme textStyles

--- frontend/src/utils/theme.ts
+++ frontend/src/utils/theme.ts
@@ -5,6 +5,21 @@ const config : ThemeConfig = {
   useSystemColorMode: false,
 }
 
-const theme = extendTheme({ config })
+const theme = extendTheme({ 
+  config,
+  textStyles: {
+    appTitle: {
+      fontSize: "3xl",
+      fontWeight: "bold",
+      letterSpacing: "-2%",
+    },
+    // h2: {
+    //   fontSize: ["36px", "48px"],
+    //   fontWeight: "semibold",
+    //   lineHeight: "110%",
+    //   letterSpacing: "-1%",
+    // },
+  },
+})
 
 export default theme
