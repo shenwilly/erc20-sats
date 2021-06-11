16bd342fa4a6e99c73f6cfd95a648feee79fb664
willy <shen.willy95@gmail.com>
2021-06-11 23:50:10 +0700

    add token constants

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,1 +1,5 @@
 export const INFURA_ID = process.env.REACT_APP_INFURA_ID;
+export const WBTC_ADDRESS = "";
+export const SATS_ADDRESS = "";
+export const WBTC_DECIMALS = 8;
+export const SATS_DECIMALS = 10;
