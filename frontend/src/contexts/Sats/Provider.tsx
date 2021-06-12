84b22d5a88df0823014b63149296fd57946ed9f3
willy <shen.willy95@gmail.com>
2021-06-12 21:48:06 +0700

    update allowance

--- frontend/src/contexts/Sats/Provider.tsx
+++ frontend/src/contexts/Sats/Provider.tsx
@@ -2,7 +2,7 @@ import { BigNumber, providers } from "ethers";
 import React, { useCallback, useState, useEffect } from "react";
 import { SATS_ADDRESS, SATS_DECIMALS, WBTC_ADDRESS, WBTC_DECIMALS } from "../../constants";
 import useWeb3 from "../../hooks/useWeb3";
-import { getBalance } from "../../utils/web3";
+import { getAllowance, getBalance } from "../../utils/web3";
 import Context from "./Context";
 
 const Provider: React.FC = ({ children }) => {
@@ -22,8 +22,8 @@ const Provider: React.FC = ({ children }) => {
     
     useEffect(() => {
         if (accountAddress && injectedProvider) {
-            fetchBalances("0xccf4429db6322d5c611ee964527d42e5d685dd6a", injectedProvider);
-            let refreshInterval = setInterval(() => fetchBalances("0xccf4429db6322d5c611ee964527d42e5d685dd6a", injectedProvider), 10000);
+            fetchBalances(accountAddress, injectedProvider);
+            let refreshInterval = setInterval(() => fetchBalances(accountAddress, injectedProvider), 10000);
             // let refreshInterval = setInterval(() => fetchBalances(accountAddress, injectedProvider), 10000);
             return () => clearInterval(refreshInterval);
         }
--- frontend/src/utils/web3.ts
+++ frontend/src/utils/web3.ts
@@ -2,28 +2,35 @@ import { ethers, providers } from "ethers";
 import { IERC20 } from "../types";
 import IERC20abi from "../contracts/abi/IERC20.json"
 
+export const getERC20 = (
+    tokenAddress: string, 
+    provider: providers.Provider
+): IERC20 => {
+    return (new ethers.Contract(tokenAddress, IERC20abi, provider)) as IERC20;
+};
+
 export const getAllowance = async (
     userAddress: string, 
     spenderAddress: string, 
     tokenAddress: string, 
     provider: providers.Provider
 ): Promise<string> => {
-    // try {
-    //   const tokenContract = getERC20Contract(provider, tokenAddress);
-    //   const allowance: string = await tokenContract.methods.allowance(userAddress, spenderAddress).call();
-    //   return allowance;
-    // } catch (e) {
-    //   return "0";
-    // }
-    return "0";
-  };
+    const tokenContract = getERC20(tokenAddress, provider);
+    try {
+        const allowance: string = (await tokenContract.allowance(userAddress, spenderAddress)).toString();
+        return allowance;
+    } catch (e) {
+        console.log(e)
+        return "0";
+    }
+};
   
 export const getBalance = async (
     userAddress: string,
     tokenAddress: string, 
     provider: providers.Provider 
 ): Promise<string> => {
-    const tokenContract = (new ethers.Contract(tokenAddress, IERC20abi, provider)) as IERC20
+    const tokenContract = getERC20(tokenAddress, provider);
     try {
         const balance: string = (await tokenContract.balanceOf(userAddress)).toString();
         return balance;
