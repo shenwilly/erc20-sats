cc3140036e0a05f2a7f53a74bf16e1323da37ca6
willy <shen.willy95@gmail.com>
2021-06-14 09:39:52 +0700

    update deploy config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,6 +6,7 @@ import { HardhatUserConfig } from "hardhat/types";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
 import "@nomiclabs/hardhat-etherscan";
+import "hardhat-gas-reporter";
 // TODO: reenable solidity-coverage when it works
 // import "solidity-coverage";
 
@@ -13,7 +14,12 @@ const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
 const RINKEBY_PRIVATE_KEY =
   process.env.RINKEBY_PRIVATE_KEY! ||
   "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3"; // well known private key
+const MAINNET_PRIVATE_KEY = 
+  process.env.MAINNET_PRIVATE_KEY! ||
+  "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3";
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
@@ -32,6 +38,10 @@ const config: HardhatUserConfig = {
       url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
       accounts: [RINKEBY_PRIVATE_KEY],
     },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [MAINNET_PRIVATE_KEY],
+    },
     coverage: {
       url: "http://127.0.0.1:8555", // Coverage launches its own ganache-cli client
     },
@@ -41,6 +51,12 @@ const config: HardhatUserConfig = {
     // Obtain one at https://etherscan.io/
     apiKey: ETHERSCAN_API_KEY,
   },
+  gasReporter: {
+    enabled: true,
+    currency: 'USD',
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    gasPrice: 12
+  }
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -6,7 +6,10 @@ async function main() {
   const factory = (await ethers.getContractFactory("SatsV1")) as SatsV1__factory;
 
   // If we had constructor arguments, they would be passed into deploy()
-  let contract = await factory.deploy(WBTC_ADDRESS_MAINNET);
+  let contract = await factory.deploy(WBTC_ADDRESS_MAINNET, {
+    gasPrice: 10000000000,
+    nonce: 51
+  });
 
   // The address the Contract WILL have once mined
   console.log(contract.address);
