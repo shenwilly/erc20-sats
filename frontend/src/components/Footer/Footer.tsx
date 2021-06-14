436f7ef788c5e0bd340c40a388d51084245b927d
willy <shen.willy95@gmail.com>
2021-06-14 09:56:54 +0700

    add logos

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,8 +1,15 @@
-import { Container, Flex, Spacer, Button, useColorMode } from "@chakra-ui/react"
+import { Container, Flex, Spacer, Button, useColorMode, Image } from "@chakra-ui/react"
 import { FaMoon, FaSun } from "react-icons/fa";
+import GithubLogo from "../../assets/img/github.png";
+import EtherscanLogo from "../../assets/img/etherscan.png";
 
 const Footer = () => {
     const { colorMode, toggleColorMode } = useColorMode()
+    
+    const openInNewTab = (url: string) => {
+        const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+        if (newWindow) newWindow.opener = null
+    }
 
     return (
         <Container maxW="container.xl">
@@ -13,15 +20,15 @@ const Footer = () => {
                         : <FaMoon />}
                 </Button>
                 <Spacer/>
-                <Button onClick={toggleColorMode} p="2" size="md" variant="ghost">
-                    {colorMode === "light" 
-                        ? <FaSun />
-                        : <FaMoon />}
+                <Button onClick={() => openInNewTab("https://etherscan.io/address/0xa5584c19ac937771cc0f6b7c19a36c36b0baf9a0")} 
+                    p="2" size="md" variant="ghost"
+                >
+                    <Image src={EtherscanLogo} fit="contain" width="24px" />
                 </Button>
-                <Button onClick={toggleColorMode} p="2" size="md" variant="ghost">
-                    {colorMode === "light" 
-                        ? <FaSun />
-                        : <FaMoon />}
+                <Button onClick={() => openInNewTab("https://github.com/shenwilly/erc20-sats")} 
+                    p="2" size="md" variant="ghost"
+                >
+                    <Image src={GithubLogo} fit="contain" width="24px" />
                 </Button>
             </Flex>
         </Container>
